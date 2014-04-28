7a0308f665c7590423a981f850db4e28ce646577
Ilaria Brugno <i.brugno@studenti.unisa.it>
2014-04-28 10:54:07 +0200

    Login 0.1

--- MyTravelDiary/gen/it/unisa/mytraveldiary/R.java
+++ MyTravelDiary/gen/it/unisa/mytraveldiary/R.java
@@ -984,6 +984,11 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int Login=0x7f05003e;
+        public static final int LoginInput=0x7f05003f;
+        public static final int Logo=0x7f05003d;
+        public static final int Password=0x7f050040;
+        public static final int PasswordInput=0x7f050041;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -995,7 +1000,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1104,6 +1109,9 @@ containing a value of this type.
         public static final int main=0x7f0c0000;
     }
     public static final class string {
+        public static final int Login=0x7f0a000f;
+        public static final int Logo=0x7f0a000e;
+        public static final int Password=0x7f0a0010;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1144,9 +1152,9 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int hello_world=0x7f0a0011;
     }
     public static final class style {
         /** 
