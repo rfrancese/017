cee9e0289192c2b893bb9307b94768f3703fb252
Carmela Anzelmo <c.anzelmo2@studenti.unisa.it>
2014-04-30 19:23:53 +0200

    Aggiunta layout Visualizza Viaggio

--- MyTravelDiary/src/it/unisa/mytraveldiary/VisualizzaViaggioActivity.java
+++ MyTravelDiary/src/it/unisa/mytraveldiary/VisualizzaViaggioActivity.java
@@ -1,32 +1,37 @@
 package it.unisa.mytraveldiary;
 
-import java.util.ArrayList;
-
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.Gallery;
 import android.os.Build;
 
-public class SearchTravelMessageActivity extends ActionBarActivity {
-
+public class VisualizzaViaggioActivity extends ActionBarActivity {
+	
+	//variable for selection intent
+	private final int PICKER = 1;
+	//variable to store the currently selected image
+	private int currentPic = 0;
+	//gallery object
+	private Gallery picGallery;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_travel_message);
+		setContentView(R.layout.activity_visualizza_viaggio);
+		         
+		//get the gallery view
+		picGallery = (Gallery) findViewById(R.id.gallery);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
@@ -34,7 +39,7 @@ public class SearchTravelMessageActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.search_travel_message, menu);
+		getMenuInflater().inflate(R.menu.visualizza_viaggio, menu);
 		return true;
 	}
 
@@ -62,14 +67,9 @@ public class SearchTravelMessageActivity extends ActionBarActivity {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(
-					R.layout.fragment_search_travel_message, container, false);
+					R.layout.fragment_visualizza_viaggio, container, false);
 			return rootView;
 		}
-	}
-
-	public void avantiVisualizzaViaggio(View view){
-		Intent intent = new Intent(this, VisualizzaViaggioActivity.class);
-		startActivity(intent);
 	}
 
 }
