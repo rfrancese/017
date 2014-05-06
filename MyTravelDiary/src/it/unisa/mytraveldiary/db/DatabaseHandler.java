98fc651fb5443b152b14ddf31285985d1145b2a4
Carmela Anzelmo <c.anzelmo2@studenti.unisa.it>
2014-05-06 15:12:13 +0200

    Aggiunta DatabaseHandler

--- MyTravelDiary/src/it/unisa/mytraveldiary/db/DatabaseHandler.java
+++ MyTravelDiary/src/it/unisa/mytraveldiary/db/DatabaseHandler.java
@@ -1,6 +1,11 @@
 package it.unisa.mytraveldiary.db;
 
+import java.util.ArrayList;
+
+import it.unisa.mytraveldiary.entity.User;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -33,5 +38,83 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		onCreate(db);
 	}
 
+	// Metodi di modifica
+	// Aggiunge un utente
+	public void addUser(User user) {
+		SQLiteDatabase db=this.getWritableDatabase();
+		ContentValues values=new ContentValues();
+		
+		values.put(U_USERNAME, user.getUsername());
+		values.put(U_PASSWORD, user.getPassword());
+		
+		db.insert(TABLE_USERS, null, values);
+		db.close();
+	}
+	
+	// Aggiorna un utente
+	public int updateUser(User user) {
+		SQLiteDatabase db=this.getWritableDatabase();
+		ContentValues values=new ContentValues();
+		values.put(U_USERNAME, user.getUsername());
+		values.put(U_PASSWORD, user.getPassword());
+		values.put(U_ID, user.getId());
+		
+		return db.update(TABLE_USERS, values, U_ID+" = ?", new String[] {String.valueOf(user.getId())});
+	}
+	
+	// Cancella un utente 
+	public void deleteUser(User user) {
+		SQLiteDatabase db=this.getWritableDatabase();
+		db.delete(TABLE_USERS, U_ID+" = ?", new String[] {String.valueOf(user.getId())});
+		db.close();
+	}
+	
+	// Metodi di accesso
+	// Ritorna un utente
+	public User getUser(int id) {
+		SQLiteDatabase db=this.getReadableDatabase();
+		
+		Cursor cursor=db.query(TABLE_USERS, new String[] {U_USERNAME, 
+								U_PASSWORD, U_ID}, U_ID+"=?",
+								new String[] {String.valueOf(id)}, null, null, null, null);
+		
+		if (cursor!=null)
+			cursor.moveToFirst();
+		
+		User user=new User(cursor.getString(0), cursor.getString(1), Integer.parseInt(cursor.getString(2)));
+		
+		return user;
+	}
+	
+	// Ritorna tutti gli utenti
+	public ArrayList<User> getAllUsers() {
+		ArrayList<User> userList=new ArrayList<User>();
+		String selectQuery="SELECT * FROM "+TABLE_USERS;
+		SQLiteDatabase db=this.getWritableDatabase();
+		Cursor cursor=db.rawQuery(selectQuery, null);
+		
+		if (cursor.moveToFirst()) {
+			do {
+				User user=new User();
+				user.setUsername(cursor.getString(0));
+				user.setPassword(cursor.getString(1));
+				user.setId(Integer.parseInt(cursor.getString(2)));
+				userList.add(user);
+			} while (cursor.moveToNext());
+		}
+		
+		return userList;
+	}
+	
+	// Ritorna il numero di utenti
+	public int getUsersCount() {
+		String countQuery="SELECT * FROM "+TABLE_USERS;
+		SQLiteDatabase db=this.getReadableDatabase();
+		Cursor cursor=db.rawQuery(countQuery, null);
+		cursor.close();
+		
+		return cursor.getCount();
+	}
+	
 	
 }
